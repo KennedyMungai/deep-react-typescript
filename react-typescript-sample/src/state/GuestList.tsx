9f32bad5a5e5970b3e4391b822cba19fc8cde142
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-22 09:49:00 +0300

    Created the onClick arrow function which is set to be activated once the button is clicked

--- react-typescript-sample/src/state/GuestList.tsx
+++ react-typescript-sample/src/state/GuestList.tsx
@@ -5,12 +5,17 @@ const GuestList: React.FC = () => {
     const [name, setName] = useState<string>('');
     const [guests, setGuests] = useState<string[]>([]);
 
+    const onClick = () => {
+        setName('');
+        setGuests([...guests, name]);
+    };
+
     return (
         <>
             <h3>Guest List</h3>
             <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
 
-            <button onClick={}>Add Guest</button>
+            <button onClick={onClick}>Add Guest</button>
         </>
     )
 }
