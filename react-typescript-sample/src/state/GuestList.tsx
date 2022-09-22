73b6e3564ffcdc8169a480e2f6f07bd0695bbdf9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-22 09:56:14 +0300

    Mapped out the guests array

--- react-typescript-sample/src/state/GuestList.tsx
+++ react-typescript-sample/src/state/GuestList.tsx
@@ -13,8 +13,11 @@ const GuestList: React.FC = () => {
     return (
         <>
             <h3>Guest List</h3>
-            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            <ul>
+                {guests.map(guest => <li key={guest}>{guest}</li>)}
+            </ul>
 
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
             <button onClick={onClick}>Add Guest</button>
         </>
     )
