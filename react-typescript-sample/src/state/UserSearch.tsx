705bf7564556af7b6f6ac4cf4b18b4b1c694115b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-22 10:01:41 +0300

    Added the React.Function compponent element to the UserSearch function and added an import for the UseState react hook

--- react-typescript-sample/src/state/UserSearch.tsx
+++ react-typescript-sample/src/state/UserSearch.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
-const UserSearch = () => {
+const UserSearch: React.FC = () => {
   return (
     <div>UserSearch</div>
   )
