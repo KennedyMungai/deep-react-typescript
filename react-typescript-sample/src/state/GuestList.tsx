8b96c68f887aac5bf2b7ae2e4ffa14f1576ab8c2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-22 09:46:41 +0300

    Created the guests state and initialized it to an empty arrau of strings

--- react-typescript-sample/src/state/GuestList.tsx
+++ react-typescript-sample/src/state/GuestList.tsx
@@ -3,13 +3,14 @@ import React, { useState } from 'react';
 
 const GuestList: React.FC = () => {
     const [name, setName] = useState<string>('');
+    const [guests, setGuests] = useState<string[]>([]);
 
     return (
         <>
             <h3>Guest List</h3>
             <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
 
-            <button>Add Guest</button>
+            <button onClick={}>Add Guest</button>
         </>
     )
 }
